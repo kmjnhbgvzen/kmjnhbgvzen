8f35543ae30fff1939a5bf69d59428516ef141f9
piyush0310 <piyushkumar8256@gmail.com>
2025-12-18 21:16:23 +0530

    updated

--- src/app/(pages)/services/software-development/page.jsx
+++ src/app/(pages)/services/software-development/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Content from "./Content";
-import FAQSchema from "@/components/FAQSchema";
+import FAQSchema from "./FAQSchema";
 
 export const metadata = {
   title:
