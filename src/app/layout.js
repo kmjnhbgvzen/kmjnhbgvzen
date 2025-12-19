22fe40c24773af77b3b6307c6e45df7b12d740bc
piyush0310 <piyushkumar8256@gmail.com>
2025-12-19 22:42:20 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 import Navbar from "@/components/Navbar";
 import Topbar from "@/components/Topbar";
@@ -34,25 +35,38 @@ export default function RootLayout({ children }) {
           content="NrE50hHl6c-azA8AW6DpgA0bwM54EcK2iL3JOBRgX4Q"
         />
 
-        {/* ✅ GOOGLE ANALYTICS (GA4) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-1HZF570XML"
-        ></script>
-
-        <script
+        {/* ✅ GOOGLE TAG MANAGER */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-1HZF570XML');
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W6ZK9ZLC');
             `,
           }}
-        ></script>
+        />
+        {/* ✅ END GTM */}
       </head>
 
       <body className="bg-[#FFFAFA]">
+        {/* ✅ GOOGLE TAG MANAGER (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W6ZK9ZLC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* ✅ END GTM (noscript) */}
+
         <Topbar />
         <Navbar />
         {children}
