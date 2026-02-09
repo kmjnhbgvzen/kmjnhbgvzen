8e93c6cadbc9a6ba281e3583f4d30ce1b6ea66c5
piyush0310 <piyushkumar8256@gmail.com>
2026-02-09 15:36:03 +0530

    updated

--- src/app/(pages)/blog/[slug]/page.jsx
+++ src/app/(pages)/blog/[slug]/page.jsx
@@ -29,34 +29,5 @@ export default async function BlogPage(props) {
   const blog = blogs.find((b) => b.slug === slug);
   if (!blog) return notFound();
 
-  // Generate FAQ structured data at SERVER level (only if FAQs exist)
-  const faqStructuredData = blog.faqs && blog.faqs.length > 0 ? {
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    mainEntity: blog.faqs.map((faq) => ({
-      "@type": "Question",
-      name: faq.question,
-      acceptedAnswer: {
-        "@type": "Answer",
-        text: faq.answer,
-      },
-    })),
-  } : null;
-
-  return (
-    <>
-      {/* FAQ Schema - Added at SERVER level to prevent duplication */}
-      {faqStructuredData && (
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify(faqStructuredData),
-          }}
-        />
-      )}
-      
-      {/* Client component handles rendering and other schemas */}
-      <BlogClient blog={blog} />
-    </>
-  );
+  return <BlogClient blog={blog} />;
 }
