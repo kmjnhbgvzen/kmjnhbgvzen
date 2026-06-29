e22a4a782d0ba300861bdb32857640ee98e40409
kajal754 <kajalkumai9898@gmail.com>
2026-06-29 16:40:38 +0530

    upd

---
+++ src/app/(citypages)/ghaziabad/seo-services-in-ghaziabad/page.jsx
@@ -0,0 +1,62 @@
+import Content from "./Content";
+import Banner from "./Banner";
+import Client from "@/components/Client";
+import LovedByClients from "@/components/LovedByClients";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Portfolio from "@/components/Portfolio";
+import LandingServices from "@/components/LandingServices";
+
+export const metadata = {
+  title: "SEO Services in Ghaziabad | Affordable & Result-Driven SEO Company",
+  description:
+    "Looking for trusted SEO services in Ghaziabad? Get expert local SEO, on-page, off-page & technical SEO solutions to rank higher on Google and grow your business online.",
+  keywords:
+    "seo services in ghaziabad, seo company in ghaziabad, local seo ghaziabad, digital marketing agency ghaziabad, best seo agency near me, affordable seo services ghaziabad, website ranking services ghaziabad",
+  alternates: {
+    canonical: "https://www.zentrixinfotech.com/ghaziabad/seo-services-in-ghaziabad",
+  },
+
+  openGraph: {
+    title: "SEO Services in Ghaziabad | Affordable & Result-Driven SEO Company",
+    description:
+      "Looking for trusted SEO services in Ghaziabad? Get expert local SEO, on-page, off-page & technical SEO solutions to rank higher on Google and grow your business online.",
+    url: "https://www.zentrixinfotech.com/ghaziabad/seo-services-in-ghaziabad",
+    siteName: "Zentrix Infotech",
+    images: [
+      {
+        url: "https://www.zentrixinfotech.com/zentrix_logo.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Zentrix Infotech Logo",
+      },
+    ],
+    locale: "en_IN",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "SEO Services in Ghaziabad | Affordable & Result-Driven SEO Company",
+    description:
+      "Looking for trusted SEO services in Ghaziabad? Get expert local SEO, on-page, off-page & technical SEO solutions to rank higher on Google and grow your business online.",
+    images: ["https://www.zentrixinfotech.com/zentrix_logo.jpg"],
+  },
+
+  icons: {
+    icon: "/favicon-v2.ico",
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <Banner />
+      <Client />
+      <Content />
+      <WhyChooseUs />
+      <LandingServices />
+      <Portfolio />
+      <LovedByClients />
+    </>
+  );
+}
